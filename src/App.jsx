ee61dcf42c8dc012548d4f30b4248ab0b5750071
LarissaVictoria9 <larissa102victoria@gmail.com>
2025-07-07 14:49:12 -0300

    codigo correto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import carregarProdutos  from './api.js';
-import AdicionarProduto from './api2.jsx';
-import './App.css';
+import Css from './css.jsx';
 import './index.css';
+import  Home from './componentes.jsx';
+import React from 'react';
 
 
  export default function App() {
@@ -18,39 +19,18 @@ import './index.css';
     fetchdados();
   }, []);
 
-  return (
-    <Router>
-    <nav>
-      <Link to="/">Página inicial</Link> | 
-      <Link to="/adicionar">Adicionar Produto</Link>
-    </nav>
-
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <>
-      <div id="titulo">
-        <p id="tt">Seja Bem-vindo à melhor loja de roupas do nordeste</p>
-        <p id="loja">STUDIO YAY</p>
-        <p id="t2">Uma loja de roupas e acessórios ideal para você!!</p>
-      </div>
-
-      <div id="produtos">
-        {produtos.map((produto) => (
-          <div key={produto.id} className="roupa">
-            <img src={produto.imagem} alt={produto.nome} />
-            <p><strong>{produto.nome}</strong></p>
-            <p>{produto.descricao}</p>
-            <p>Preço: R$ {produto.preco.toFixed(2)}</p>
-          </div>
-        ))}
-      </div>
-    </>
-}
-/>
-<Route path="/adicionar" element={<AdicionarProduto />} />
-      </Routes>
-    </Router>
-  );
-}
+    return (
+      <Router>
+      <nav>
+        <Link to="/">Página inicial</Link> 
+      </nav>
+  
+      <Routes>
+        <Route
+          path="/"
+          element={<Css produtos={produtos} />}/>
+        </Routes>
+      </Router>
+    );
+   }
+    
