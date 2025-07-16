fbc889634c2f1fa2be389957b0957bee9bb4a0c4
webystenia <railwnetp@gmail.com>
2025-07-16 19:48:11 -0300

    Adicionei tela de cadastro e edição de produtos

---
+++ src/pages/data.jsx
@@ -0,0 +1,83 @@
+// src/data.js
+export const produtos = [
+  {
+    id: 1,
+    nome: "Camiseta Básica",
+    preco: 29.90,
+    descricao: "Camiseta de algodão, confortável e durável.",
+    categoria: "Roupas",
+    imagem: "https://http2.mlstatic.com/camiseta-branca-lisa-basica-tradicional-camisa-malha-algodo-D_NQ_NP_491111-MLB20481948828_112015-F.jpg"
+  },
+  {
+    id: 2,
+    nome: "Calça Jeans",
+    preco: 89.90,
+    descricao: "Calça jeans clássica, perfeita para o dia a dia.",
+    categoria: "Roupas",
+    imagem: "https://www.impecavel.com.br/media/catalog/product/cache/1/image/85e4522595efc69f496374d01ef2bf13/8/5/851422-1.jpg"
+  },
+  {
+    id: 3,
+    nome: "Jaqueta Jeans",
+    preco: 179.90,
+    descricao: "Jaqueta jeans com forro interno, ideal para o frio.",
+    categoria: "Roupas",
+    imagem: "https://newscotidiano.com.br/wp-content/uploads/2022/07/Como-combinar-jaqueta-jeans_-Reproducao-Canva.jpg"
+  },
+  {
+    id: 4,
+    nome: "Vestido Floral",
+    preco: 99.90,
+    descricao: "Vestido leve com estampa floral para dias quentes.",
+    categoria: "Roupas",
+    imagem: "https://images.tcdn.com.br/img/img_prod/413300/vestido_kethy_sb25914_floral_119197_1_44f185597b549c3569e69bdc5dee447d.jpeg"
+  },
+  {
+    id: 5,
+    nome: "Blusa de Manga Longa",
+    preco: 49.90,
+    descricao: "Blusa confortável ideal para o outono.",
+    categoria: "Roupas",
+    imagem: "https://cdnv2.moovin.com.br/monjua/imagens/produtos/det/181634_INTEIRA_1006240950506217.jpg"
+  },
+  {
+    id: 6,
+    nome: "Shorts Jeans",
+    preco: 59.90,
+    descricao: "Shorts jeans moderno com barra desfiada.",
+    categoria: "Roupas",
+    imagem: "https://i5.walmartimages.com/asr/cf3eab13-9422-41c5-b553-0574dc68ce62.7102054b38e3a023b85563e7254a01b8.jpeg"
+  },
+  {
+    id: 7,
+    nome: "Camisa Social",
+    preco: 89.90,
+    descricao: "Camisa social branca para eventos e trabalho.",
+    categoria: "Roupas",
+    imagem: "https://images.tcdn.com.br/img/img_prod/914261/camisa_masculina_social_manga_longa_rosa_1451_1_0f03aa646ba6d32f4f9cb3aa49833634.jpg"
+  },
+  {
+    id: 8,
+    nome: "Saia Plissada",
+    preco: 79.90,
+    descricao: "Saia plissada elegante para looks casuais ou formais.",
+    categoria: "Roupas",
+    imagem: "https://www.luxwoman.pt/wp-content/uploads/2020/09/sacoor-59.50.jpg"
+  },
+  {
+    id: 9,
+    nome: "Casaco de Lã",
+    preco: 249.90,
+    descricao: "Casaco de lã quentinho para o inverno.",
+    categoria: "Roupas",
+    imagem: "https://mlb-s1-p.mlstatic.com/casaco-de-l-com-capuz-feminino-2015-inverno-frete-gratis-755101-MLB20274407357_042015-F.jpg"
+  },
+  {
+    id: 10,
+    nome: "Jaqueta Corta-Vento",
+    preco: 129.90,
+    descricao: "Jaqueta leve resistente à água e vento.",
+    categoria: "Roupas",
+    imagem: "https://images.tcdn.com.br/img/img_prod/753613/jaqueta_corta_vento_masculino_light_lets_surf_wss_brasil_13601_1_595a4f5153cdad847ada8edc08da5643.jpg"
+  }
+];
